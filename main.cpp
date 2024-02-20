7754b60d85c8ab485d6d8ecca468720a64fa39a3
enkg60 <emiliomateoguevara@gmail.com>
2024-02-19 20:06:07 -0500

    Worked on main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 usine namespace std;
 
 int main(){
+  SDL_Window* window = nullptr;
+  SDL_Renderer* renderer = nullptr;
+  
+  SDL_Init(SDL_INIT_VIDEO);
+  SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
 
+  SDL_SetRenderDrawColor(renderer,0,0,0,255);
+  SDL_RenderClear(renderer);
+
+  SDL_SetRenderDrawColor(renderer,255,255,255,255);
+  SDL_RenderDrawPoint(renderer,640/2,480/2);
+
+  SDL_RenderPresent(renderer);
+
+  SDL_Delay(10000);
+
+  SDL_Quit();
+
+  return 0;
 }
